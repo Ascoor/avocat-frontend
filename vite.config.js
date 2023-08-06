7f66a50a724f3124ece47e98b97c2eb52d6b3f19
ascar <askar_17@yahoo.com>
2023-08-06 19:14:13 +0200

    zxxzsd

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   server: {
-    host: '127.0.0.1',
     port: 3000,
   },
   css: {
-    modules: true, // If you want to use CSS modules
+    modules: true,
   },
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500,
+  },
+  base: 'https://avocat.live', // اسم النطاق هنا يكون https://example.com/subfolder/
 });
