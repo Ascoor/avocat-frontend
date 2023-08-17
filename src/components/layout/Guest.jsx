9408e9da604d1e2a8ec8a5f23cb65494fe992a24
askar <mr.askar1984@gmail.com>
2023-08-17 21:35:27 +0300

    c

--- src/components/layout/Guest.jsx
+++ src/components/layout/Guest.jsx
@@ -3,10 +3,11 @@ import { useTransition, useSpring, animated } from "@react-spring/web";
 import { Card, Button, Container, Spinner } from "react-bootstrap";
 import { RiLoginCircleLine, RiUserAddLine } from "react-icons/ri";
 import { Collapse } from "bootstrap/dist/js/bootstrap.bundle";
-import '../../assets/css/Welcome.css';
+
 import logo from "../../images/logo512.png";
 import patternLogo from "../../images/welcome.jpg";
 import patternLogoSmall from "../../images/logo2.png";
+import LawyerServices from "./LawyerService.component";
 
 const Login = React.lazy(() => import("../auth/login"));
 const Register = React.lazy(() => import("../auth/register"));
@@ -15,12 +16,6 @@ const Guest = () => {
   const [showLoginForm, setShowLoginForm] = useState(false);
   const [showRegisterForm, setShowRegisterForm] = useState(false);
   const [showLogoAndButtons, setShowLogoAndButtons] = useState(true);
-  const [showCard, setShowCard] = useState(false);
-
-  const companyAnimation = useSpring({
-    from: { transform: "translateY(100%)" },
-    to: { transform: showCard ? "translateY(0)" : "translateY(100%)" },
-  });
   const handleCloseForm = () => {
     setShowLoginForm(false);
     setShowRegisterForm(false);
@@ -66,18 +61,7 @@ const Guest = () => {
     },
     config: { duration: 500 },
   });
-  useEffect(() => {
-    // ... باقي الكود
-    
-    // عند فتح الصفحة، قم بتفعيل الكارت بعد فترة زمنية
-    const cardActivationTimeout = setTimeout(() => {
-      setShowCard(true);
-    }, 1000); // انتظر 1 ثانية قبل عرض الكارت
-  
-    return () => {
-      clearTimeout(cardActivationTimeout); // تنظيف ال timeout عندما يتم إزالة المكون
-    };
-  }, []);
+
   useEffect(() => {
     const mainNav = document.querySelector("#mainNav");
 
@@ -91,13 +75,11 @@ const Guest = () => {
 
         const navbarItems = navbarCollapse.querySelectorAll("a");
 
-        const closeNavbarOnClick = () => {
-          collapse.hide();
-        };
-
         // Closes responsive menu when a scroll trigger link is clicked
         for (let item of navbarItems) {
-          item.addEventListener("click", closeNavbarOnClick);
+          item.addEventListener("click", function (event) {
+            collapse.hide();
+          });
         }
       }
 
@@ -137,16 +119,15 @@ const Guest = () => {
   return (
     <>
       <header
-  className="masthead"
-  style={{
-    backgroundImage: `url(${patternLogo})`,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    height: "100vh", // اضبط ارتفاع الصفحة هنا
-  }}
->
+        className="masthead"
+        style={{
+          backgroundImage: `url(${patternLogo})`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          height: "100vh",
+        }}
+      >
         <div className="navbar navbar-light fixed-top" id="mainNav">
-   
           <div className="container">
             <img src={logo} width="118" height="54" alt="Logo" />
             <button
@@ -170,10 +151,16 @@ const Guest = () => {
             </div>
           </div>
         </div>
-        <div className="d-flex flex-column justify-content-center align-items-center" style={{ height: "100%" }}>
-  {showLogoAndButtons && (
-    <animated.div style={logoAnimation} className="logo-container">
-         <img
+        <div
+          className="d-flex flex-column justify-content-center align-items-center"
+          style={{
+            paddingLeft: "10px",
+            height: "100%",
+          }}
+        >
+          {showLogoAndButtons && (
+            <animated.div style={logoAnimation} className="logo-container">
+              <img
                 src={patternLogoSmall}
                 alt="Pattern Logo"
                 className="logo img-fluid"
@@ -181,9 +168,9 @@ const Guest = () => {
               />
             </animated.div>
           )}
-       {showLogoAndButtons && (
-    <div className="mt-4 d-flex justify-content-center">
-      <Button
+          {showLogoAndButtons && (
+            <div className="mt-4 d-flex justify-content-center">
+              <Button
                 variant="success"
                 onClick={handleShowLoginForm}
                 style={{ marginRight: "10px" }}
@@ -197,27 +184,21 @@ const Guest = () => {
               </Button>
             </div>
           )}
-              {showLogoAndButtons && (
-            <animated.div style={companyAnimation}>
-          <div className="centered-column">
-  <div className="glass-card text-center" style={{ width: "18rem", marginBottom: "20px" }}>
-    <div className="card-body">
-
-             <h5 className="glass-card-title">أفوكات</h5>
-             <h6 className="glass-card-title-small">مكتب محاماة</h6>
-             <p className="glass-card-text">
-          
+          {showLogoAndButtons && (
+            <div className="col-lg-8 mx-auto text-center mt-4">
+              <strong>
+                <span style={{ color: "#ffffff" }}>
+                  <p>
                     تعد أفوكات أول منظومة تغير قواعد العمل في مجال المحاماة، حيث تساهم
                     أدواتها التكنولوجية المتقدمة في تبسيط سير العمل وإدارة القضايا بكفاءة
                     وتقديم خدمات استثنائية للعملاء. اكتشف مستقبل ممارسة المحاماة مع
                     الحل الرقمي الشامل المقدم من أفوكات.
                   </p>
-                </div>
-              </div>
-              </div>
-          </animated.div>
+                </span>
+              </strong>
+            </div>
           )}
-   {formsTransition((styles, item) =>
+          {formsTransition((styles, item) =>
             item ? (
               <animated.div style={{ ...styles, width: "100%", marginTop: "20px" }}>
                 <div className="d-flex justify-content-center">
@@ -244,11 +225,19 @@ const Guest = () => {
         </div>
       </header>
 
+      <LawyerServices />
 
-
-      <footer>
-           <Container id="footer" className="footer-container " fluid>
-          <p>
+      <footer
+        style={{
+          background: "linear-gradient(rgb(11 22 26), rgb(14 48 66), rgb(10 18 24))",
+          direction: "rtl",
+          color: "#fff",
+          textAlign: "center",
+          padding: "10px 0",
+        }}
+      >
+        <Container>
+          <p style={{ margin: 0 }}>
             &copy; {new Date().getFullYear()} Avocat All rights reserved
           </p>
         </Container>
