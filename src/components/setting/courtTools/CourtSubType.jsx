8a0c330c7660ba302e9f9c6f7bf29b139878107c
askar <mr.askar1984@gmail.com>
2023-09-12 06:18:06 +0300

    asd

--- src/components/setting/courtTools/CourtSubType.jsx
+++ src/components/setting/courtTools/CourtSubType.jsx
@@ -6,9 +6,9 @@ import { Row, Col, Button, Modal, Form, Card } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import axios from 'axios';
 import API_CONFIG from '../../../config';
-const CourtSubType = ({show, handleClose}) => {
+const CourtSubType = ({ show, handleClose }) => {
   const [selectedCourtTypeId, setSelectedCourtTypeId] = useState('');
-  const [newCourtSubTypeName, setNewCourtSubTypeName] = useState("");
+  const [newCourtSubTypeName, setNewCourtSubTypeName] = useState('');
   const [courtTypes, setCourtTypes] = useState([]);
   const [courtLevels, setCourtLevels] = useState([]);
   const [courts, setCourts] = useState([]);
@@ -24,13 +24,13 @@ const CourtSubType = ({show, handleClose}) => {
   const [alertVariant, setAlertVariant] = useState('success');
   const [showAddCourtSubTypeModal, setShowAddCourtSubTypeModal] =
     useState(false);
-    const [courtSubTypeAlert, setCourtSubTypeAlert] = useState(null);
-    const [alertMessage, setAlertMessage] = useState(null); // For table alerts
-    const [successMessage, setSuccessMessage] = useState(null); // For table success
-    const [message, setMessage] = useState(null); // For table success
-    const [modalMessage, setModalMessage] = useState(null); // For modal alerts
-    const valueIsValid = !!newCourtSubTypeName.trim(); // true if newCourtTypeName is not empty
-    useEffect(() => {
+  const [courtSubTypeAlert, setCourtSubTypeAlert] = useState(null);
+  const [alertMessage, setAlertMessage] = useState(null); // For table alerts
+  const [successMessage, setSuccessMessage] = useState(null); // For table success
+  const [message, setMessage] = useState(null); // For table success
+  const [modalMessage, setModalMessage] = useState(null); // For modal alerts
+  const valueIsValid = !!newCourtSubTypeName.trim(); // true if newCourtTypeName is not empty
+  useEffect(() => {
     fetchCourtSubTypes();
     fetchCourtTypes();
   }, []);
@@ -60,15 +60,15 @@ const CourtSubType = ({show, handleClose}) => {
   }, [alertMessage]);
   const fetchCourtSubTypes = async () => {
     try {
-        const response = await axios.get(
-            `${API_CONFIG.baseURL}/api/court_sub_types/`
-        );
-        setCourtSubTypes(response.data);
+      const response = await axios.get(
+        `${API_CONFIG.baseURL}/api/court_sub_types/`
+      );
+      setCourtSubTypes(response.data);
     } catch (error) {
-        setError("حدث خطأ في استرجاع أنواع المحاكم الفرعية");
-        console.error("حدث خطأ في استرجاع أنواع المحاكم الفرعية: ", error);
+      setError('حدث خطأ في استرجاع أنواع المحاكم الفرعية');
+      console.error('حدث خطأ في استرجاع أنواع المحاكم الفرعية: ', error);
     }
-};
+  };
 
   const fetchCourtTypes = async () => {
     try {
@@ -82,43 +82,42 @@ const CourtSubType = ({show, handleClose}) => {
     }
   };
 
-
   const indexOfLastItem = currentPage * itemsPerPage;
   const indexOfFirstItem = indexOfLastItem - itemsPerPage;
   const currentItems = courtTypes.slice(indexOfFirstItem, indexOfLastItem);
   const handleAddCourtSubType = () => {
     fetch(`${API_CONFIG.baseURL}/api/court_sub_types/`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            court_type_id: newCourtTypeId,
-            name: newCourtSubTypeName,
-        }),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        court_type_id: newCourtTypeId,
+        name: newCourtSubTypeName,
+      }),
     })
-        .then((response) => response.json())
-        .then((data) => {
-            fetchCourtSubTypes();
-            setShowAddCourtSubTypeModal(false);
-            setNewCourtSubTypeName("");
-            setNewCourtTypeId("");
-            setShowAlert(true);
-            setAlertMessage("تمت إضافة نوع المحكمة الفرعية بنجاح.");
-            setTimeout(() => {
-                setShowAlert(false);
-            }, 5000);
-        })
-        .catch((error) => {
-            setError("حدث خطأ في إضافة نوع المحكمة الفرعية");
-            console.error("حدث خطأ في إضافة نوع المحكمة الفرعية: ", error);
-            setShowAlert(true);
-            setAlertMessage("فشل في إضافة نوع المحكمة الفرعية.");
-            setTimeout(() => {
-                setShowAlert(false);
-            }, 5000);
-        });
-};
+      .then((response) => response.json())
+      .then((data) => {
+        fetchCourtSubTypes();
+        setShowAddCourtSubTypeModal(false);
+        setNewCourtSubTypeName('');
+        setNewCourtTypeId('');
+        setShowAlert(true);
+        setAlertMessage('تمت إضافة نوع المحكمة الفرعية بنجاح.');
+        setTimeout(() => {
+          setShowAlert(false);
+        }, 5000);
+      })
+      .catch((error) => {
+        setError('حدث خطأ في إضافة نوع المحكمة الفرعية');
+        console.error('حدث خطأ في إضافة نوع المحكمة الفرعية: ', error);
+        setShowAlert(true);
+        setAlertMessage('فشل في إضافة نوع المحكمة الفرعية.');
+        setTimeout(() => {
+          setShowAlert(false);
+        }, 5000);
+      });
+  };
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
@@ -154,7 +153,6 @@ const CourtSubType = ({show, handleClose}) => {
             >
               <h3 style={{ color: '#006e5d' }}>أنواع المحاكم الفرعية</h3>
             </Card.Header>
-       
 
             <Card.Body>
               <Table striped bordered hover responsive>
@@ -166,42 +164,40 @@ const CourtSubType = ({show, handleClose}) => {
                   </tr>
                 </thead>
                 <tbody>
-                {currentItems.map((courtSubType) => (
-                      <tr
-                        style={{ backgroundColor: '#D1ECF1', color: '#0C5460' }}
-                        key={courtSubType.id}
-                      >
-                   
-                   <td>{courtSubType.name}</td>
-                                                <td>{courtSubType.court_type.name}</td>
+                  {currentItems.map((courtSubType) => (
+                    <tr
+                      style={{ backgroundColor: '#D1ECF1', color: '#0C5460' }}
+                      key={courtSubType.id}
+                    >
+                      <td>{courtSubType.name}</td>
+                      <td>{courtSubType.court_type_name}</td>
 
-                                             
-                        <td>
-                          <Button
-                            variant="danger"
-                            onClick={() =>
-                              handleDelete(
-                                courtSubType.id,
-                                courtSubType.name,
-                                'court_sub_types'
-                              )
-                            }
-                          >
-                            <FcFullTrash />
-                          </Button>
-                        </td>
-                      </tr>
-                    ))}
+                      <td>
+                        <Button
+                          variant="danger"
+                          onClick={() =>
+                            handleDelete(
+                              courtSubType.id,
+                              courtSubType.name,
+                              'court_sub_types'
+                            )
+                          }
+                        >
+                          <FcFullTrash />
+                        </Button>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </Table>
             </Card.Body>
             <Card.Footer>
-            <CustomPagination
-            totalCount={courtSubTypes.length}
-            itemsPerPage={itemsPerPage}
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-          />
+              <CustomPagination
+                totalCount={courtSubTypes.length}
+                itemsPerPage={itemsPerPage}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+              />
             </Card.Footer>
           </Col>
         </Row>
@@ -215,10 +211,7 @@ const CourtSubType = ({show, handleClose}) => {
             {courtTypes.length > 0 && (
               <Form.Group controlId="courtTypeId">
                 <Form.Label>نوع المحكمة:</Form.Label>
-                <Form.Control
-                  as="select"
-                  value={selectedCourtTypeId}
-                >
+                <Form.Control as="select" value={selectedCourtTypeId}>
                   <option value="">اختر نوع المحكمة</option>
                   {courtTypes.map((courtType) => (
                     <option key={courtType.id} value={courtType.id}>
@@ -235,7 +228,7 @@ const CourtSubType = ({show, handleClose}) => {
                 value={newCourtSubTypeName}
                 onChange={(e) => setNewCourtSubTypeName(e.target.value)}
               />
-                  {modalMessage && (
+              {modalMessage && (
                 <Alert variant={modalMessage.type}>{modalMessage.text}</Alert>
               )}
             </Form.Group>
