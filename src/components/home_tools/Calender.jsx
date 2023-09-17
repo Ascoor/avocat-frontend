9c6e3083f4b2487191f5b1c7c6197d31aafc8a2c
askar <mr.askar1984@gmail.com>
2023-09-17 15:11:11 +0300

    sd

--- src/components/home_tools/Calender.jsx
+++ src/components/home_tools/Calender.jsx
@@ -5,7 +5,7 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import { Card, Col, Row } from 'react-bootstrap';
 import { useSpring, animated } from '@react-spring/web';
-import '../../assets/css/Calender.css';
+import '../../assets/css/calender.css';
 
 const Calendar = () => {
   const events = [
