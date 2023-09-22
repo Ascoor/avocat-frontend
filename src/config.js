c19cee0fa7cfa706e0c808118bde633691ea1ad3
askar <mr.askar1984@gmail.com>
2023-09-22 07:55:16 +0300

    Clear Git cache

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const API_CONFIG = {
-  // baseURL  الخاص بك وفقًا لعنوان  API  قم بتعديل قيمة الـ
-  baseURL: 'https://avocat.live/avocat-api' /// Server Avocat API
-  //baseURL: 'http://localhost:8000', //
-};
-
-export default API_CONFIG;
