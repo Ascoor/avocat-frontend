cc73013546201d3b76b34880b1f6dfe810e17173
askar <mr.askar1984@gmail.com>
2024-01-22 06:12:18 +0200

    sd

--- src/components/service/index.jsx
+++ src/components/service/index.jsx
@@ -1,18 +1,21 @@
 import { useState, useEffect } from 'react';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Row, Col,InputGroup, FormControl } from 'react-bootstrap';
 import axios from 'axios';
 import ServiceModal from './ServiceModal';
 import API_CONFIG from '../../config';
 import ServiceDetailsModal from './ServiceDetailsModal';
 import SectionHeader from '../home_tools/SectionHeader';
 import { ServiceIcon } from '../../assets/icons/index';
+import CustomPagination from '../home_tools/Pagination';
 const Services = () => {
   const [services, setServices] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [editingService, setEditingService] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedService, setSelectedService] = useState(null);
   const [showDetailsModal, setShowDetailsModal] = useState(false);
-
+  const itemsPerPage = 10;
   useEffect(() => {
     // Fetch services when the component mounts
     fetchServices();
@@ -56,7 +59,19 @@ const Services = () => {
     setSelectedService(null);
     setShowDetailsModal(false); // Add this line to set showDetailsModal to false
   };
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`${API_CONFIG.baseURL}/api/service-search?search=${searchQuery}`);
+      setServices(response.data);
+      setCurrentPage(1);
+    } catch (error) {
+      console.error('Error searching cases:', error);
+    }
+  };
 
+
+
+  const paginatedServices = services.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
   return (
     <>
       <SectionHeader
@@ -65,9 +80,22 @@ const Services = () => {
         setShowAddModal={handleAddService}
         icon={ServiceIcon}
       />
-
-      <Card className="m-4">
-        <Card.Body>
+      <Card>
+      <Row>
+            <Col xs={12} md={6} lg={4}>
+              <InputGroup className="mb-3">
+                <FormControl
+                  placeholder="بحث..."
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <Button variant="outline-secondary" onClick={handleSearch}>
+                  بحث
+                </Button>
+              </InputGroup>
+            </Col>
+          </Row>
+              <Card.Body>
           <div className="table-responsive">
             <table className="special-table">
               <thead>
@@ -81,13 +109,28 @@ const Services = () => {
                 </tr>
               </thead>
               <tbody>
-                {services.map((service) => (
+                {paginatedServices.map((service) => (
                   <tr key={service.id}>
-                    <td>{service.service_no}</td>
-                    <td>{service.service_name}</td>
-                    <td>{service.client?.name || service.unclient?.name}</td>
-                    <td>{service.service_place}</td>
-                    <td>{service.service_status}</td>
+                    <td>{service.slug}</td>
+                    <td>{service.service_type?.name}</td>
+                    <td>
+  {service.clients && service.clients.length > 0
+    ? service.clients.map((client, index) => (
+        <span key={index}>
+          {client.name}{index < service.clients.length - 1 ? ', ' : ''}
+        </span>
+      ))
+    : service.unclients && service.unclients.map((unclient, index) => (
+        <span key={index}>
+          {unclient.name}{index < service.unclients.length - 1 ? ', ' : ''}
+        </span>
+      ))
+  }
+</td>
+
+
+                    <td>{service.service_place_name}</td>
+                    <td>{service.status}</td>
                     <td>
                       <Button
                         variant="primary"
@@ -117,6 +160,14 @@ const Services = () => {
             </table>
           </div>
         </Card.Body>
+        <Card.Footer>
+          <CustomPagination
+            totalCount={services.length}
+            itemsPerPage={itemsPerPage}
+            currentPage={currentPage}
+            onPageChange={setCurrentPage}
+          />
+        </Card.Footer>
       </Card>
       {showDetailsModal && (
         <ServiceDetailsModal
