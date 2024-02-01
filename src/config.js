e11791c0586cfba6eb5a7a889be9768931821706
askar <mr.askar1984@gmail.com>
2024-02-01 04:33:06 +0200

    sd

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const API_CONFIG = {
   
-baseURL: 'https://avocat.live/avocat-api' /// Server Avocat API
+baseURL: 'http://avocat.live/avocat-api' /// Server Avocat API
 // baseURL: 'http://localhost:8000', //
 };
 
