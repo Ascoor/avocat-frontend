cfe7b56b61510024344cc64ec97c0fd7bafe6b73
root <root@DESKTOP-KVH5ITH.>
2025-02-14 11:44:11 +0200

    gdfsسيسي

--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -1,10 +1,9 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useSidebar } from "../../utils/SidebarContext";
+import { useSpring, animated } from "@react-spring/web";
 
-import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSidebar } from '../../utils/SidebarContext';
-import { useSpring, animated } from '@react-spring/web';
-
-import { LogoArt, LogoSuit } from '../../assets/images/index';
+import { LogoArt, LogoSuit } from "../../assets/images/index";
 import {
   FaHome,
   FaBars,
@@ -15,50 +14,22 @@ import {
   FaBriefcase,
   FaBalanceScale,
   FaMoneyBillWave,
-  FaUserAltSlash,
-  FaUserTie,
-} from 'react-icons/fa';
-import { IoMdClose } from 'react-icons/io';
+} from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 
 const Sidebar = () => {
   const { isSidebarOpen, setIsSidebarOpen, isMobile } = useSidebar();
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
   const menuItems = [
-    { to: '/', icon: <FaHome />, label: 'الرئيسية' },
-    { to: '/legcases', icon: <FaFolder />, label: 'القضايا' },
-    {
-      to: '/clients',
-      icon: <FaUsers />,
-      label: 'العملاء',
-    },
-    { to: '/legcase-services', icon: <FaCogs />, label: 'الخدمات' },
-    { to: '/invoices', icon: <FaFileInvoice />, label: 'الفواتير' },
-    { to: '/consultations', icon: <FaBalanceScale />, label: 'الاستشارات' },
-    { to: '/expenses', icon: <FaMoneyBillWave />, label: 'المصروفات' },
-    { to: '/search-courts-api', icon: <FaBriefcase />, label: 'بحث المحاكم' },
-    {
-      to: '/managment-settings',
-      icon: <FaCogs />,
-      label: 'إدارة المكتب',
-      subItems: [
-        {
-          to: '/managment-settings/lawyers',
-          label: 'المحامون',
-          icon: <FaUsers />,
-        },
-        {
-          to: '/managment-settings/procedures',
-          label: 'الإجراءات',
-          icon: <FaUsers />,
-        },
-        {
-          to: '/managment-settings/courts',
-          label: 'المحاكم',
-          icon: <FaUsers />,
-        },
-      ],
-    },
+    { to: "/", icon: <FaHome />, label: "الرئيسية" },
+    { to: "/legcases", icon: <FaFolder />, label: "القضايا" },
+    { to: "/clients", icon: <FaUsers />, label: "العملاء" },
+    { to: "/legcase-services", icon: <FaCogs />, label: "الخدمات" },
+    { to: "/invoices", icon: <FaFileInvoice />, label: "الفواتير" },
+    { to: "/consultations", icon: <FaBalanceScale />, label: "الاستشارات" },
+    { to: "/expenses", icon: <FaMoneyBillWave />, label: "المصروفات" },
+    { to: "/contracts", icon: <FaBriefcase />, label: "العقود" },
   ];
 
   const [openSubMenu, setOpenSubMenu] = useState(null);
@@ -68,35 +39,36 @@ const Sidebar = () => {
   };
 
   const sidebarAnimation = useSpring({
-    width: isSidebarOpen ? '16rem' : '4rem',
+    width: isSidebarOpen ? "16rem" : "4rem",
     config: { tension: 210, friction: 20 },
   });
 
   return (
-    <div className="flex">
+    <>
+      {/* الشريط الجانبي */}
       <animated.div
         style={sidebarAnimation}
-        className="fixed top-0 right-0 h-full bg-gradient-to-b from-avocat-blue-light via-avocat-indigo to-gray-300 dark:bg-gradient-to-t dark:from-avocat-indigo-darker dark:via-avocat-blue-darker dark:to-avocat-blue shadow-lg z-30 hidden md:flex flex-col"
+       className="fixed top-0 right-0 h-full bg-gradient-to-b from-avocat-blue-light via-avocat-indigo to-gray-300 dark:bg-gradient-to-b dark:from-avocat-blue-darker dark:via-avocat-indigo-darker dark:to-gray-800 shadow-lg z-30 flex flex-col transition-all"
       >
-        {/* Logo */}
+        {/* الشعار */}
         <div className="flex items-center justify-center h-16">
-          {isSidebarOpen ? (
-            <img src={LogoArt} alt="Logo Art" className="w-28 h-14" />
-          ) : (
-            <img src={LogoSuit} alt="Logo Suit" className="mt-4 w-12 h-12" />
-          )}
+          <img
+            src={isSidebarOpen ? LogoArt : LogoSuit}
+            alt="Logo"
+            className={isSidebarOpen ? "w-28 h-14" : "w-12 h-12 mt-2"}
+          />
         </div>
 
-        {/* Menu */}
-        <ul className="mt-8 flex-1">
+        {/* القائمة */}
+        <ul className="mt-4 flex-1">
           {menuItems.map((item, index) => (
             <li key={index} className="group relative">
-              <NavLink
+                 <NavLink
                 to={item.to}
                 onClick={() => item.subItems && handleToggleSubMenu(index)}
                 className={`flex items-center p-3 rounded-lg transition-colors duration-300 ${
                   isSidebarOpen
-                    ? 'space-x-4 text-gray-100 hover:bg-pink-300 hover:text-blue-900 dark:hover:bg-avocat-indigo-light'
+                    ? 'space-x-4 text-gray-100 hover:bg-pink-800 hover:text-avocat-orange-light dark:hover:bg-avocat-indigo-light'
                     : 'justify-center'
                 } ${
                   openSubMenu === index
@@ -105,94 +77,32 @@ const Sidebar = () => {
                 }`}
               >
                 <span className="text-xl">{item.icon}</span>
-                {isSidebarOpen && <span className="flex-1">{item.label}</span>}
+                {isSidebarOpen &&          <span
+                  className="flex-1 text-gray-800 dark:text-gray-100 text-center "
+       
+                >
+                  {item.label}
+                </span>}
               </NavLink>
 
-              {/* Sub-menu */}
-              {item.subItems && openSubMenu === index && (
-                <ul
-                  className={`mt-2 space-y-2 ${
-                    isSidebarOpen ? 'pl-10' : 'flex flex-col items-center'
-                  }`}
-                >
-                  {item.subItems.map((subItem, subIndex) => (
-                    <li key={subIndex}>
-                      <NavLink
-                        to={subItem.to}
-                        className={`flex items-center p-2 text-gray-100 hover:bg-pink-300 dark:hover:bg-avocat-indigo-light ${
-                          isSidebarOpen ? 'space-x-4' : 'justify-center'
-                        }`}
-                      >
-                        <span className="text-xl">{subItem.icon}</span>
-                        {isSidebarOpen && <span>{subItem.label}</span>}
-                      </NavLink>
-                    </li>
-                  ))}
-                </ul>
+              {/* عرض اسم العنصر عند المرور عليه في الوضع المصغر */}
+              {!isSidebarOpen && (
+                <span className="absolute right-full top-1/2 transform -translate-y-1/2 bg-gray-800 text-white px-3 py-1 text-sm rounded-lg opacity-0 group-hover:opacity-100 transition-opacity">
+                  {item.label}
+                </span>
               )}
             </li>
           ))}
         </ul>
-
-        {/* Toggle Button */}
+ 
         <button
           onClick={toggleSidebar}
-          className="absolute bottom-4 right-4 p-2 bg-red-700 text-white rounded-full hover:bg-indigo-500 transition-colors"
+          className="absolute bottom-4 right-4 p-2 bg-red-700 text-white rounded-full hover:bg-indigo-500 transition"
         >
           {isSidebarOpen ? <IoMdClose /> : <FaBars />}
         </button>
       </animated.div>
-
-      {/* Mobile Sidebar */}
-      {isMobile && (
-        <div className="md:hidden">
-          {isSidebarOpen && (
-            <div className="fixed top-0 left-0 w-full h-full bg-gradient-to-t from-avocat-blue-dark via-avocat-indigo-dark to-avocat-orange dark:bg-gradient-to-t dark:from-avocat-indigo-darker dark:via-avocat-blue-dark dark:to-avocat-indigo-darker text-white z-40">
-              <div className="flex items-center justify-between p-4">
-                <button
-                  onClick={toggleSidebar}
-                  className="text-2xl focus:outline-none"
-                >
-                  <IoMdClose />
-                </button>
-              </div>
-              <ul className="flex flex-col items-center space-y-4">
-                {menuItems.map((item, index) => (
-                  <li key={index} className="w-full text-center">
-                    <NavLink
-                      to={item.to}
-                      className="flex flex-col items-center w-full p-4 hover:bg-gray-700 focus:outline-none"
-                    >
-                      <span className="text-3xl mb-2">{item.icon}</span>
-                      <span className="text-lg">{item.label}</span>
-                    </NavLink>
-
-                    {/* عرض الأيقونات الفرعية في الشريط الجوال */}
-                    {item.subItems && (
-                      <ul className="mt-2 space-y-2">
-                        {item.subItems.map((subItem, subIndex) => (
-                          <li key={subIndex} className="w-full">
-                            <NavLink
-                              to={subItem.to}
-                              className="flex flex-col items-center p-4 hover:bg-pink-300 dark:hover:bg-avocat-indigo-light text-center"
-                            >
-                              <span className="text-2xl mb-1">
-                                {subItem.icon}
-                              </span>
-                              <span className="text-sm">{subItem.label}</span>
-                            </NavLink>
-                          </li>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
+    </>
   );
 };
 
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,38 +1,29 @@
-import React, { Suspense } from 'react';
+import React, { useEffect } from 'react';
 import Sidebar from '../components/dashboard/Sidebar';
 import Header from '../components/dashboard/Header';
 import { useSidebar } from '../utils/SidebarContext';
 import AuthRoutes from '../components/layout/AuthRoutes'; 
-
 const AuthWrapper = () => {
-  const { isSidebarOpen, isMobile } = useSidebar();
-
+  const { isSidebarOpen, isMobile } = useSidebar();  
+  const sidebarWidth = !isMobile ? (isSidebarOpen ? '16rem' : '4rem') : '0';
+ 
   return (
-    <div className="flex flex-col md:flex-row h-screen bg-gray-50 dark:bg-gray-900 transition-all duration-300">
-      {/* Sidebar */}
-      {!isMobile && (
-        <div className={`transition-all z-50 duration-300 ${isSidebarOpen ? 'w-64' : 'w-16'}`}>
-          <Sidebar />
-        </div>
-      )}
-
-      {/* Main Content */}
-      <div className="flex-1 flex z-50 flex-col">
+    <div className="flex flex-col md:flex-row h-screen font-['cairo'] bg-gray-50 dark:bg-gray-900 transition-all duration-500 ease-in-out">
+      <div style={{ width: sidebarWidth }} className="transition-all duration-500 ease-in-out">
+        <Sidebar />
+      </div>
+      <div className="flex-1 flex flex-col transition-all duration-500 ease-in-out">
         <Header />
-        
-        {/* Main Layout */}
-        <div className="flex-1 overflow-y-auto p-4  bg-white dark:bg-gray-800">
-          <main className="max-w-7xl mx-auto ">
-            <div className="p-4 sm:p-6 lg:p-8 rounded shadow-md transition-shadow duration-300 ease-in-out hover:shadow-lg">
-              
-              {/* ✅ Lazy Loaded Routes with Suspense Fallback */}
-               <AuthRoutes />
-             
-              
+        <div className="flex-1 overflow-y-auto p-4">
+          <main className="grow">
+            <div className="w-full max-w-9xl mx-auto">
+              <div className="card-main p-4 sm:p-6 lg:p-8 rounded bg-white mt-12 dark:bg-gray-800 shadow-md relative transition-shadow duration-500 ease-in-out hover:shadow-lg">
+                <AuthRoutes />
+              </div>
             </div>
           </main>
         </div>
-      </div> 
+      </div>
     </div>
   );
 };
